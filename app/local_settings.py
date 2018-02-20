bdef57c63a05bbc5d34513c07ca06144a587914e
aimodding <aimodding@gmail.com>
2018-02-20 13:25:17 +0300

    conf commit

--- app/local_settings.py
+++ app/local_settings.py
@@ -9,12 +9,17 @@ DEBUG = True
 
 # DO NOT use Unsecure Secrets in production environments
 # Generate a safe one with:
-#     python -c "import os; print repr(os.urandom(24));"
+#    python -c "import os; print(repr(os.urandom(24)));"
 SECRET_KEY = 'DO_NOT_use_Unsecure_Secrets_in_production_environments'
 
 # SQLAlchemy settings
 SQLALCHEMY_DATABASE_URI = 'sqlite:///../app.sqlite'
 SQLALCHEMY_TRACK_MODIFICATIONS = False    # Avoids a SQLAlchemy Warning
+
+BABEL_TRANSLATION_DIRECTORIES = os.path.join(os.path.abspath(
+    os.path.dirname(__file__)), 'translations/')
+
+BABEL_DEFAULT_LOCALE = 'ru'
 
 # Flask-Mail settings
 # For smtp.gmail.com to work, you MUST set "Allow less secure apps"
@@ -22,10 +27,6 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False    # Avoids a SQLAlchemy Warning
 # Change it in https://myaccount.google.com/security#connectedapps
 # (near the bottom).
 
-BABEL_TRANSLATION_DIRECTORIES = os.path.join(os.path.abspath(
-    os.path.dirname(__file__)), 'translations/')
-
-BABEL_DEFAULT_LOCALE = 'ru'
 
 MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 587
